4f8564a35abbc4a95d4893d758e8b47b37eaf41b
TrigamDev <trigambusiness@gmail.com>
2025-01-12 10:42:30 -0500

    Oh yeah, that's not necessary

--- src/main/java/dev/trigam/field/event/EventInit.java
+++ src/main/java/dev/trigam/field/event/EventInit.java
@@ -4,28 +4,6 @@ public class EventInit {
 
     public static void init () {
 
-//        PlayerPickItemEvents.BLOCK.register( ( player, pos, state, includeData ) -> {
-//            if ( state.getBlock() instanceof HangingBannerBlock hangingBanner ) {
-//
-//                Block returnBlock = HangingBannerBlock.asBanner( hangingBanner );
-//
-//                BlockEntity hangingBannerEntity = player.getServerWorld().getBlockEntity( pos );
-//                ComponentMap components = ComponentMap.EMPTY;
-//                if ( hangingBannerEntity != null ) {
-//                    components = hangingBannerEntity.createComponentMap();
-//                }
-//
-//                ItemStack returnItem = new ItemStack( returnBlock.asItem() );
-//                returnItem.applyComponentsFrom( components );
-//
-//                Field.LOGGER.info(String.valueOf(returnItem));
-//
-//                return returnItem;
-//            }
-//
-//            return null;
-//        });
-
     }
 
 }
